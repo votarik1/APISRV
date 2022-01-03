6a84ca2be73e38408a36026b3c353e8f2050e8ad
Александр <alexspb41@gmail.com>
2022-01-03 12:14:05 +0300

    ADD HW1

---
+++ HW1/ValuesHolder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    public class ValuesHolder
+    {
+        public Dictionary<DateTime,string> Values { get; set; }
+
+        public ValuesHolder()
+        {
+            Values = new Dictionary<DateTime, string>();
+        }
+
+        public Dictionary<DateTime, string> Get() => Values;
+
+        public void ADD(DateTime time, string val) => Values.Add(time, val);
+    }
+}
